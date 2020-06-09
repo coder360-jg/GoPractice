9401f554bce462d6ce591d3db5df6eab1a5146c2
jyoti <jyotigupt.08@gmail.com>
2020-06-09 11:56:23 +0530

    Method : function with receiver

--- 5_methods/main.go
+++ 5_methods/main.go
@@ -6,12 +6,26 @@ import "fmt"
 
 type myInt int
 
+type empData struct {
+	empid   int
+	empname string
+}
+
 func (n myInt) Sqr() myInt {
 	return n * n
+}
+
+func (e1 empData) empInfo() empData {
+	e1.empid = 005
+	e1.empname = "Eva"
+	return e1
 }
 
 func main() {
 	f := (myInt)(5)
 	fmt.Println(f.Sqr())
+
+	e := empData{001, "Alexa"}
+	fmt.Println(e.empInfo())
 
 }
